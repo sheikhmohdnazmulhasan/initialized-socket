027e3b70483a4aa3c733611f675bd1480cb460dc
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-11-14 15:05:50 +0600

    client cdn added into client/index.html

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-import { createServer } from "http";
-import { Server } from "socket.io";
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 
 const httpServer = createServer();
 const socket = new Server(httpServer, {});
